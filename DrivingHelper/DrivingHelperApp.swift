bbed3439561edc79b489c12ecc4d1deba8a5dbb3
brian419 <json10@crimson.ua.edu>
2022-12-30 16:19:44 -0600

    Initial Commit

---
+++ DrivingHelper/DrivingHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrivingHelperApp.swift
+//  DrivingHelper
+//
+//  Created by Brian Son on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct DrivingHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
